dd3033fdc898e7887c303cb3360059fc50cc8599
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-14 16:16:50 -0500

    Update Automovil.java

--- src/test/java/test/vehiculos/Automovil.java
+++ src/test/java/test/vehiculos/Automovil.java
@@ -1,4 +1,18 @@
 package test.vehiculos;
 
 public class Automovil extends Vehiculo {
+    private int puestos;
+
+    public Automovil(String placa, String nombre, double precio, double peso, Fabricante fabricante, int puestos){
+        this.placa = placa;
+        this.puertas = 4;
+        this.velocidadMaxima = 100;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.peso = peso;
+        this.traccion = "FWD";
+        this.fabricante = fabricante;
+        this.puestos = puestos;
+        Vehiculo.cantidadVehiculos ++;
+    }
 }
