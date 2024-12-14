cfa56b93265c9545c996945bb5ec43056210a9f5
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-14 16:16:36 -0500

    Update Fabricante.java

--- src/test/java/test/vehiculos/Fabricante.java
+++ src/test/java/test/vehiculos/Fabricante.java
@@ -1,4 +1,17 @@
 package test.vehiculos;
 
 public class Fabricante {
+    private String nombre;
+    private Pais pais;
+
+    public Fabricante(String nombre, Pais pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public void setNombre(String nombre){this.nombre = nombre;}
+    public void setPais(Pais pais){this.pais = pais;}
+
+    public String getNombre(){return nombre;}
+    public Pais getPais(){return pais;}
 }
