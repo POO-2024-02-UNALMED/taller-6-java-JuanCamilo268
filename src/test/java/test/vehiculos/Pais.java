b862b965e96dd2b0f89f25e8c0be23e4990aca8a
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-14 16:16:32 -0500

    Update Pais.java

--- src/test/java/test/vehiculos/Pais.java
+++ src/test/java/test/vehiculos/Pais.java
@@ -1,4 +1,12 @@
 package test.vehiculos;
 
 public class Pais {
+    private String nombre;
+
+    public Pais(String nombre){
+        this.nombre = nombre;
+    }
+    public void setNombre(String nombre){this.nombre = nombre;}
+
+    public String getNombre(){return nombre;}
 }
