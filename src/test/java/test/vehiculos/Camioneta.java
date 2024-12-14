82115acf3278faa2c1c4be41639eaa89273480da
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-14 16:16:40 -0500

    Update Camioneta.java

--- src/test/java/test/vehiculos/Camioneta.java
+++ src/test/java/test/vehiculos/Camioneta.java
@@ -1,4 +1,22 @@
 package test.vehiculos;
 
 public class Camioneta extends Vehiculo{
+    private boolean volco;
+
+    public Camioneta(String placa, int puertas, String nombre, double precio, double peso, Fabricante fabricante, boolean volco) {
+        this.placa = placa;
+        this.puertas = puertas;
+        this.velocidadMaxima = 90;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.peso = peso;
+        this.traccion = "4X4";
+        this.fabricante = fabricante;
+        this.volco = volco;
+        Vehiculo.cantidadVehiculos++;
+    }
+
+    public void setVolco(boolean volco){this.volco = volco;}
+
+    public boolean isVolco(){return volco;}
 }
