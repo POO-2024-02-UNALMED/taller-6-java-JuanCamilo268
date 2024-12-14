0483f6ccb66fdcd7d2a2cd3bce7f193e26977109
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-14 17:26:47 -0500

    Update Fabricante.java

--- src/test/java/test/vehiculos/Fabricante.java
+++ src/test/java/test/vehiculos/Fabricante.java
@@ -1,12 +1,21 @@
 package test.vehiculos;
 
 public class Fabricante {
+    private static Fabricante[] lista = new Fabricante[0];
     private String nombre;
     private Pais pais;
+    private int ventas;
 
     public Fabricante(String nombre, Pais pais) {
         this.nombre = nombre;
         this.pais = pais;
+        int x = lista.length;
+        Fabricante[] A = new Fabricante[x+1];
+        for (int i = 0; i < lista.length + 1; i++ ){
+            A[i] = lista[i];
+        }
+        A[x] = this;
+        lista = A;
     }
 
     public void setNombre(String nombre){this.nombre = nombre;}
@@ -14,4 +23,19 @@ public class Fabricante {
 
     public String getNombre(){return nombre;}
     public Pais getPais(){return pais;}
+
+    public void venta(){
+        ventas ++;
+        pais.exportacion ++;
+    }
+    public static Fabricante fabricaMayorVendedor(){
+        Fabricante potencia = lista[0];
+        for (Fabricante fabrica: lista) {
+            if (fabrica.ventas > potencia.ventas) {
+                potencia = fabrica;
+            }
+        }
+        return potencia;
+    }
 }
+
