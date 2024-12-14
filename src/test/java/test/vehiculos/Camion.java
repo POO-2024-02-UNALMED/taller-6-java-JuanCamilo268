6ffed2e691777bfab14cd6127e36d9f19045e148
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-14 16:16:45 -0500

    Update Camion.java

--- src/test/java/test/vehiculos/Camion.java
+++ src/test/java/test/vehiculos/Camion.java
@@ -1,4 +1,22 @@
 package test.vehiculos;
 
 public class Camion extends Vehiculo{
+    private int ejes;
+
+    public Camion(String placa, String nombre, double precio, double peso, Fabricante fabricante, int ejes) {
+        this.placa = placa;
+        this.puertas = 2;
+        this.velocidadMaxima = 80;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.peso = peso;
+        this.traccion = "4X2";
+        this.fabricante = fabricante;
+        this.ejes = ejes;
+        cantidadVehiculos++;
+    }
+
+    public void setEjes(int ejes){this.ejes = ejes;}
+
+    public int getEjes(){return ejes;}
 }
