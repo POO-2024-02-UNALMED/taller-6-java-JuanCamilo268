caae1604603a4c90143b9502bbb77228831c3e7f
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-15 11:52:07 -0500

    Update Camioneta.java

--- src/test/java/test/vehiculos/Camioneta.java
+++ src/test/java/test/vehiculos/Camioneta.java
@@ -4,6 +4,7 @@ import vehiculos.Fabricante;
 
 public class Camioneta extends Vehiculo{
     private boolean volco;
+    protected static int cantidadVehiculos;
 
     public Camioneta(String placa, int puertas, String nombre, double precio, double peso, Fabricante fabricante, boolean volco) {
         this.placa = placa;
@@ -15,8 +16,8 @@ public class Camioneta extends Vehiculo{
         this.traccion = "4X4";
         this.fabricante = fabricante;
         this.volco = volco;
-        Vehiculo.otro();
-        Vehiculo.camioneta();
+        cantidadVehiculos ++;
+        this.fabricante.venta();
     }
 
     public void setVolco(boolean volco){this.volco = volco;}
